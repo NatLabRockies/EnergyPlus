e7279d1dc63da6bd973450363f07e25cf331ea37
rraustad <rraustad@fsec.ucf.edu>
2024-12-24 19:17:56 -0500

    expm1

--- src/EnergyPlus/VariableSpeedCoils.cc
+++ src/EnergyPlus/VariableSpeedCoils.cc
@@ -8256,7 +8256,7 @@ namespace VariableSpeedCoils {
         To1 = aa + LatentCapacityTimeConstant;
         Error = 1.0;
         while (Error > 0.001) {
-            To2 = aa - LatentCapacityTimeConstant * (std::exp(-To1 / LatentCapacityTimeConstant) - 1.0);
+            To2 = aa - LatentCapacityTimeConstant * std::expm1(-To1 / LatentCapacityTimeConstant);
             Error = std::abs((To2 - To1) / To1);
             To1 = To2;
         }
