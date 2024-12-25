62fd1a45bc619ecd9260d9ce56f27459b2793d11
rraustad <rraustad@fsec.ucf.edu>
2024-12-24 19:38:28 -0500

    expm1

--- src/EnergyPlus/WaterToAirHeatPumpSimple.cc
+++ src/EnergyPlus/WaterToAirHeatPumpSimple.cc
@@ -3768,7 +3768,7 @@ namespace WaterToAirHeatPumpSimple {
         To1 = aa + LatentCapacityTimeConstant;
         Error = 1.0;
         while (Error > 0.001) {
-            To2 = aa - LatentCapacityTimeConstant * (std::exp(-To1 / LatentCapacityTimeConstant) - 1.0);
+            To2 = aa - LatentCapacityTimeConstant * std::expm1(-To1 / LatentCapacityTimeConstant);
             Error = std::abs((To2 - To1) / To1);
             To1 = To2;
         }
