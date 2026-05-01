3aefb2654c423908fcef7afefd1011f2cac96a82
kevin-moos <kevin.moos@bigladdersoftware.com>
2026-05-01 09:28:38 -0500

    Rename field to Objective Function to Optimize

--- src/EnergyPlus/HybridEvapCoolingModel.hh
+++ src/EnergyPlus/HybridEvapCoolingModel.hh
@@ -263,7 +263,7 @@ namespace HybridEvapCoolingModel {
         Constant::eFuel secondFuel = Constant::eFuel::Invalid; // Second fuel type
         Constant::eFuel thirdFuel = Constant::eFuel::Invalid;  // Third fuel type
         ObjectiveFunctionType ObjectiveFunction =
-            ObjectiveFunctionType::ElectricityUse; // Objective function to minimize, currently only Electricity Use and Supply Temperature work
+            ObjectiveFunctionType::ElectricityUse; // Objective function to optimize, currently only Electricity Use and Supply Temperature work
 
         int UnitOn;                          // feels like it should be a bool but its an output and I couldn't get it to work as a bool
         Real64 UnitTotalCoolingRate;         // unit output to zone, total cooling rate [W]
