fe1ebab9d8881ab28cfdcbdb627d5697886e4a75
Rick Strand <rkstrand@illinois.edu>
2026-08-28 05:54:24 -0500

    OpTemp Kiva Crash Initial Solution
    Using OpTemp control with Kiva and requesting the Zone Thermostat Operative Temperature output variable causes a hard crash because the ZnAirRpt structure has not been allocated yet.  This solution moves the allocation up to right after the zones and spaces have been processed.

--- src/EnergyPlus/HeatBalanceManager.cc
+++ src/EnergyPlus/HeatBalanceManager.cc
@@ -2086,6 +2086,12 @@ namespace HeatBalanceManager {
 
         // Now get Space data after Zones are set up, because Space is optional, Zones are not
         GetSpaceData(state, ErrorsFound);
+
+        // Following used for reporting--moved here to avoid a crash in Kiva that sets up a report variable that uses ZnAirRpt
+        state.dataHeatBal->ZnAirRpt.allocate(state.dataGlobal->NumOfZones);
+        if (state.dataHeatBal->doSpaceHeatBalanceSizing || state.dataHeatBal->doSpaceHeatBalanceSimulation) {
+            state.dataHeatBal->spaceAirRpt.allocate(state.dataGlobal->numSpaces);
+        }
     }
 
     void GetIncidentSolarMultiplier(EnergyPlusData &state, bool &ErrorsFound)
